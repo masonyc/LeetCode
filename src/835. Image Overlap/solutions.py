3090bf983dc9f23ca03501be7bf21914460b70a0
Mason(Yun) Chen <mason.yunchen@gmail.com>
2020-03-17 20:35:43 +1300

    add commit on Image Overlap

--- src/835. Image Overlap/solutions.py
+++ src/835. Image Overlap/solutions.py
@@ -9,12 +9,15 @@ class Solution(object):
         :rtype: int
         """
         N = len(A)
+        # i / N 是行, i % N 是列
+        # i / N * 100 + i % N 为了确保在Map里保持唯一性
         LA = [i / N * 100 + i % N
               for i in list(range(N * N))
               if A[i / N][i % N]]
         LB = [i / N * 100 + i % N
               for i in list(range(N * N))
               if B[i / N][i % N]]
+        # i-j 是总距离 如果多个（i-j）的总距离相同 代表他们移动后重叠
         counter = Counter(i-j
                           for i in LA
                           for j in LB)
