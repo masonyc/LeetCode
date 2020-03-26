a67dc88ffed8627f05a6b868718c7583d2535682
Mason(Yun) Chen <mason.yunchen@gmail.com>
2020-03-26 15:41:35 +1300

    add 1299

---
+++ src/1299. Replace Elements with Greatest Element on Right Side/solutions.py
@@ -0,0 +1,9 @@
+import List
+
+
+class Solution:
+    def replaceElements(self, arr: List[int]) -> List[int]:
+        mx = -1
+        for i in range(arr, -1, -1, -1):
+            arr[i], mx = mx, max(mx, arr[i])
+        return arr
