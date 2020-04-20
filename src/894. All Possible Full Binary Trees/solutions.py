924a3663471c264b49cf0420ac1e985352ba9261
Mason(Yun) Chen <mason.yunchen@gmail.com>
2020-04-20 18:43:35 +1200

    added 1305,814,894

---
+++ src/894. All Possible Full Binary Trees/solutions.py
@@ -0,0 +1,25 @@
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, x):
+#         self.val = x
+#         self.left = None
+#         self.right = None
+
+
+class Solution:
+    def __init__(self):
+        self.res = {0: [], 1: [TreeNode(0)]}
+
+    def allPossibleFBT(self, N: int) -> List[TreeNode]:
+        if N not in self.res:
+            ans = []
+            for x in range(N):
+                y = N - 1 - x
+                for left in self.allPossibleFBT(x):
+                    for right in self.allPossibleFBT(y):
+                        bns = TreeNode(0)
+                        bns.left = left
+                        bns.right = right
+                        ans.append(bns)
+            self.res[N] = ans
+        return self.res[N]
